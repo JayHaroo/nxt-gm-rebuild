fb3b19889fdd8f5ddae30cc2427fe050a4727a5c
M-Jey <mjeyict3@gmail.com>
2025-03-27 15:34:57 +0800

    enhance Landing component with login and community join buttons

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,9 +1,26 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 
-export default function Landing(){
-    return(
-        <View className='flex-1 justify-center items-center bg-[#121212]'>
-            <Image source={require('../assets/logo-with-text.png')} />
+export default function Landing() {
+  return (
+    <View className="flex-1 items-center justify-center bg-[#121212]">
+      <View className="top-[-200px] p-[110px]">
+        <Image source={require('../assets/logo-with-text.png')} className="object-contain" />
+      </View>
+
+      <View className="flex items-center">
+        <Pressable className="mt-5 w-[160px] items-center rounded-xl bg-green-700">
+          <Text className="p-3 text-white">Login</Text>
+        </Pressable>
+
+        <View className='p-5 items-center'>
+            <Text className="text-white z-10 bg-[#121212] top-3 w-[30px] text-center">OR</Text>
+          <View className="w-[200px] border border-white z-0" />
         </View>
-    )
+
+        <Pressable>
+          <Text className="text-white">Join the community</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
