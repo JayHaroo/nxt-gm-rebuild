3345fa2f94bcff7eecab9ae21cd94cd92b3bd8e8
M-Jey <mjeyict3@gmail.com>
2025-03-27 15:41:57 +0800

    implement Login component with email and password fields

--- components/elements/Login.tsx
+++ components/elements/Login.tsx
@@ -1,6 +1,25 @@
+import { View, Text, TextInput, Pressable } from 'react-native';
+
 export default function Login() {
-    return(
-        <>
-        </>
-    )
+  return (
+    <View className="mt-5 items-center">
+      <Text className="mb-5 text-xl font-bold">Welcome!</Text>
+
+      <TextInput
+        placeholder="Email"
+        className="mb-3 w-[250px] rounded-xl border-2 border-black p-3 outline-none"
+        keyboardType="email-address"
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        className="mb-3 w-[250px] rounded-xl border-2 border-black p-3 outline-none"
+      />
+      <Pressable
+        className="w-[200px] items-center rounded-xl bg-green-700" // Trigger FetchAccount display
+      >
+        <Text className="p-3 text-white">Login</Text>
+      </Pressable>
+    </View>
+  );
 }
