48b73ecea3a79b2930bb81760cb3598e5c0605e4
M-Jey <mjeyict3@gmail.com>
2025-04-05 13:27:19 +0800

    add registration functionality and enhance landing page with login/register toggle

---
+++ components/elements/Register.tsx
@@ -0,0 +1,25 @@
+import { View, TextInput, Text, Pressable } from "react-native";
+
+export default function Register() {
+    return (
+        <View className="mt-5 items-center">
+            <Text className="mb-5 text-xl font-bold">Join our Community!</Text>
+
+            <TextInput
+                placeholder="Email"
+                className="mb-3 w-[250px] rounded-xl border-2 border-black p-3"
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <TextInput
+                placeholder="Password"
+                secureTextEntry
+                className="mb-3 w-[250px] rounded-xl border-2 border-black p-3"
+            />
+
+            <Pressable className="mt-5 w-[250px] items-center rounded-xl bg-green-700">
+                <Text className="p-3 text-white">Register</Text>
+            </Pressable>
+        </View>
+    )
+}
