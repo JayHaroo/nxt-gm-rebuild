944a20389be032600089e47a3ba4f58905226ec2
M-Jey <mjeyict3@gmail.com>
2025-04-06 16:31:44 +0800

    add navigation functionality to Home screen with a Go Back button

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
+  const navigation = useNavigation();
+  const goBack = () => {
+    navigation.goBack();
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-[#121212]">
       <Text className="text-white">Welcome to the Home Screen!</Text>
+      <Pressable onPress={goBack} className="mt-4 bg-blue-500 p-2 rounded">
+        <Text className="text-white">Go Back</Text>
+      </Pressable>
     </View>
   );
 }
