86c65b1925625b23222093cd2d288bebb5342aeb
M-Jey <mjeyict3@gmail.com>
2025-04-06 16:29:40 +0800

    implement navigation with React Navigation and set up Home screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
-import Landing from 'components/Landing';
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import './global.css';
+
+import Home from 'components/pages/Home';
+import Landing from 'components/Landing';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator 
+      screenOptions={{
+        headerShown: false
+      }}>
+      <Stack.Screen name="Landing" component={Landing} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
-      <Landing />
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
     </>
   );
 }
