5380256ddd985c716448b9e8cbeb2d531bfb532b
M-Jey <mjeyict3@gmail.com>
2025-04-15 16:02:04 +0800

    fix: format date display for post details and improve layout consistency

--- components/pages/post.tsx
+++ components/pages/post.tsx
@@ -31,7 +31,7 @@ export default function Post() {
     <>
       <View className="h-full items-center bg-[#121212] px-4 py-2 pt-10">
         <View className="flex-row items-center justify-between bg-[#121212]">
-          <View className="flex-row justify-between bg-[#121212] w-full">
+          <View className="w-full flex-row justify-between bg-[#121212]">
             <View className="flex-row items-center">
               <Image
                 source={require('../../assets/logo.png')}
@@ -43,19 +43,37 @@ export default function Post() {
             </Pressable>
           </View>
         </View>
+
         <View className="items-center justify-between bg-[#121212] px-4 py-2 pt-10">
-          {details.map((detail) => (
-            <View
-              key={detail._id}
-              className="flex-col items-center justify-between bg-[#121212] px-4 py-2 pt-10">
-              <Text className="text-[30px] font-extrabold text-white">{detail.title}</Text>
-              <Text className="text-white font-bold"> Posted By: {detail.author?.username} at {detail.createdAt}</Text>
-              <Text className="text-white text-[20px] pt-5">{detail.desc}</Text>
-              {detail.image_uri && (
-                <Image src={detail.image_uri} className="w-[350px] h-[500px] object-contain" />
-              )}
-            </View>
-          ))}
+          {details.map((detail) => {
+            const formattedDate = new Date(detail.createdAt).toLocaleString('en-US', {
+              weekday: 'short',
+              year: 'numeric',
+              month: 'short',
+              day: 'numeric',
+              hour: '2-digit',
+              minute: '2-digit',
+              hour12: true,
+            });
+
+            return (
+              <View
+                key={detail._id}
+                className="flex-col items-center justify-between bg-[#121212] px-4 py-2 pt-10">
+                <Text className="text-[30px] font-extrabold text-white">{detail.title}</Text>
+                <Text className="font-bold text-white">
+                  Posted By: {detail.author?.username} at {formattedDate}
+                </Text>
+                <Text className="pt-5 text-[20px] text-white">{detail.desc}</Text>
+                {detail.image_uri && (
+                  <Image
+                    src={detail.image_uri}
+                    className="h-[500px] w-[350px] object-contain"
+                  />
+                )}
+              </View>
+            );
+          })}
         </View>
       </View>
     </>
