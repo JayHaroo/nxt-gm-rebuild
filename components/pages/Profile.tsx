5e15bbc9b94ce28f6e1dc8de41131179baab2fd5
M-Jey <mjeyict3@gmail.com>
2025-04-16 12:46:43 +0800

    feat: add post retrieval by author ID and display in Profile component

--- components/pages/Profile.tsx
+++ components/pages/Profile.tsx
@@ -2,16 +2,35 @@ import { View, Text, Pressable, Image, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Entypo from '@expo/vector-icons/Entypo';
+import { useEffect, useState } from 'react';
 
 export default function Profile() {
   const route = useRoute();
   const userid = route.params?.userid ?? 'User';
+  const username = route.params?.username ?? 'User';
   const navigation = useNavigation();
   const goBack = () => {
     navigation.goBack();
   };
+
+  const [posts, setPosts] = useState([]);
+
+  const fetchPost = async () => {
+    try {
+      const response = await fetch(`http://192.168.56.1:3000/api/post/${userid}`);
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error('Error fetching feed:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
   return (
-    <View className='h-full items-center bg-[#121212] px-4 py-2 pt-10'>
+    <View className="h-full items-center bg-[#121212] px-4 py-2 pt-10">
       <View className="items-center justify-between bg-[#121212]">
         <View className="w-full flex-row justify-between bg-[#121212]">
           <View className="flex-row items-center">
@@ -27,9 +46,31 @@ export default function Profile() {
           </Pressable>
         </View>
 
-        <View className='items-center justify-between bg-[#121212] mt-5'>
-            <Text className="text-xl font-bold text-white">Profile</Text>
-            <Text className="text-lg font-bold text-white">User ID: {userid}</Text>
+        <View className="mt-5 items-center justify-between bg-[#121212]">
+          <Text className="text-xl font-bold text-white">Profile</Text>
+          <Text className="text-sm font-bold text-white">User ID: {userid}</Text>
+          <Text className="text-3xl font-bold text-white">{username}</Text>
+        </View>
+
+        <View className="mt-5 w-max bg-[#121212] px-4">
+          {posts.length > 0 ? (
+            <ScrollView
+              className="w-full"
+              contentContainerStyle={{ paddingBottom: 20 }}
+              showsVerticalScrollIndicator={false}>
+              {posts.map((post) => (
+                <Pressable
+                  key={post._id}
+                  onPress={() => navigation.navigate('Post', { postId: post._id })}
+                  className="rounded-2xl bg-[#1e1e1e] p-4 w-[300px] shadow-md items-center ">
+                  <Text className="mb-1 text-lg font-semibold text-white">{post.title}</Text>
+                  <Text className="text-gray-400">{post.desc}</Text>
+                </Pressable>
+              ))}
+            </ScrollView>
+          ) : (
+            <Text className="text-center text-base text-white">No posts available</Text>
+          )}
         </View>
       </View>
     </View>
