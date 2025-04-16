08d8c7bc620fd1352a1b7388522c7ae201cc0ba5
M-Jey <mjeyict3@gmail.com>
2025-04-16 13:02:46 +0800

    fix: center-align post title and description in Profile component

--- components/pages/Profile.tsx
+++ components/pages/Profile.tsx
@@ -65,8 +65,8 @@ export default function Profile() {
                   key={post._id}
                   onPress={() => navigation.navigate('Post', { postId: post._id })}
                   className="rounded-2xl bg-[#1e1e1e] p-4 w-[300px] shadow-md items-center ">
-                  <Text className="mb-1 text-lg font-semibold text-white">{post.title}</Text>
-                  <Text className="text-gray-400">{post.desc}</Text>
+                  <Text className="mb-1 text-lg font-semibold text-white text-center">{post.title}</Text>
+                  <Text className="text-gray-400 text-center">{post.desc}</Text>
                 </Pressable>
               ))}
             </ScrollView>
