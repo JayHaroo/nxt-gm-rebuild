6e3365a4573cd12d92730bda819e697b9318ace7
M-Jey <mjeyict3@gmail.com>
2025-04-16 11:54:30 +0800

    feat: enhance Home and Post components with icon buttons and layout improvements

--- components/pages/post.tsx
+++ components/pages/post.tsx
@@ -1,7 +1,8 @@
-import { View, Text, Pressable, TextInput, Image, Alert } from 'react-native';
+import { View, Text, Pressable, TextInput, Image, Alert, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useState, useEffect } from 'react';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function Post() {
   const navigation = useNavigation();
@@ -40,52 +41,56 @@ export default function Post() {
                 className="h-[8px] w-[100px] object-contain"
               />
             </View>
-            <Pressable onPress={goBack} className="mr-2 items-center rounded bg-green-700">
-              <Text className="p-3 text-white">Go Back</Text>
+            <Pressable
+              onPress={goBack}
+              className="mr-2 h-[50px] w-[50px] items-center items-center justify-center rounded bg-green-700 align-middle">
+              <Entypo name="back" size={24} color="white" />
             </Pressable>
           </View>
         </View>
 
-        <View className="items-center justify-between bg-[#121212] px-4 py-2 pt-10">
-          {details.map((detail) => {
-            const formattedDate = new Date(detail.createdAt).toLocaleString('en-US', {
-              weekday: 'short',
-              year: 'numeric',
-              month: 'short',
-              day: 'numeric',
-              hour: '2-digit',
-              minute: '2-digit',
-              hour12: true,
-            });
+        <ScrollView className='w-full'>
+          <View className="items-center justify-between bg-[#121212]">
+            {details.map((detail) => {
+              const formattedDate = new Date(detail.createdAt).toLocaleString('en-US', {
+                weekday: 'short',
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: true,
+              });
 
-            return (
-              <View
-                key={detail._id}
-                className="flex-col items-center justify-between bg-[#121212] px-4 py-2 pt-10">
-                <Text className="text-[30px] font-extrabold text-white">{detail.title}</Text>
-                <Text className="font-bold text-white">
-                  Posted By: {detail.author?.username} at {formattedDate}
-                </Text>
-                <Text className="pt-5 text-[20px] text-white">{detail.desc}</Text>
-                {detail.image_uri && (
-                  <Image src={detail.image_uri} className="h-[500px] w-[350px] object-contain" />
-                )}
-              </View>
-            );
-          })}
-        </View>
-        <View className="flex-col items-center justify-between bg-[#121212] px-4 py-2 pt-10">
-          { isLiked ? (
-            <Pressable onPress={() => setIsLiked(false)}>
-              <AntDesign name="like1" size={40} color="white" />
-            </Pressable>
-          ) : (
-            <Pressable onPress={() => setIsLiked(true)}>
-              <AntDesign name="like2" size={40} color="white" />
-            </Pressable>
-          )}
-          <Text className="text-[20px] font-bold text-white p-5">Comments:</Text>
-        </View>
+              return (
+                <View
+                  key={detail._id}
+                  className="flex-col items-center justify-between bg-[#121212] px-4 py-2 pt-10">
+                  <Text className="text-[30px] font-extrabold text-white text-center">{detail.title}</Text>
+                  <Text className="font-bold text-gray-400">
+                    Posted By: {detail.author?.username} at {formattedDate}
+                  </Text>
+                  <Text className="pt-5 text-[20px] text-white text-center p-7">{detail.desc}</Text>
+                  {detail.image_uri && (
+                    <Image src={detail.image_uri} className="h-[500px] w-[350px] object-contain" />
+                  )}
+                </View>
+              );
+            })}
+          </View>
+          <View className="flex-col items-center justify-between bg-[#121212] px-4 py-2 pt-10">
+            {isLiked ? (
+              <Pressable onPress={() => setIsLiked(false)}>
+                <AntDesign name="like1" size={40} color="white" />
+              </Pressable>
+            ) : (
+              <Pressable onPress={() => setIsLiked(true)}>
+                <AntDesign name="like2" size={40} color="white" />
+              </Pressable>
+            )}
+            <Text className="p-5 text-[20px] font-bold text-white">Comments:</Text>
+          </View>
+        </ScrollView>
       </View>
     </>
   );
