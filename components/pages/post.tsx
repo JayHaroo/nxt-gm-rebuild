c99d810c248eddffb72897e272f29ee7db3db04d
M-Jey <mjeyict3@gmail.com>
2025-04-19 13:52:21 +0800

    fix: pass username to Post component when navigating and log user details

--- components/pages/post.tsx
+++ components/pages/post.tsx
@@ -30,6 +30,8 @@ export default function Post() {
           } else {
             setOwner(false);
           }
+
+          console.log('User details:', username);
         }
       } catch (error) {
         console.error('Error fetching feed:', error);
