5f4c28c6d3e16b748c78eb56485d797583c55687
M-Jey <mjeyict3@gmail.com>
2025-04-21 22:09:16 +0800

    feat: add adaptive icon to Landing component and improve layout styling

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { StatusBar, statusBar } from 'expo-status-bar';
 import './global.css';
 
 import Home from 'components/pages/Home';
@@ -13,16 +13,19 @@ const Stack = createNativeStackNavigator();
 
 function RootStack() {
   return (
-    <Stack.Navigator 
-      screenOptions={{
-        headerShown: false
-      }}>
-      <Stack.Screen name="Landing" component={Landing} />
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Create" component={Create} />
-      <Stack.Screen name="Post" component={Post} />
-      <Stack.Screen name="Profile" component={Profile} />
-    </Stack.Navigator>
+    <>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Create" component={Create} />
+        <Stack.Screen name="Post" component={Post} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
