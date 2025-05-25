1f0405e6f6a7cecebc51826398b02658bb79c25f
JayHaroo <saturnomjey09@gmail.com>
2025-05-25 18:53:38 +0800

    Add loading indicator to Login component and improve error handling

--- components/elements/Login.tsx
+++ components/elements/Login.tsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
-import { View, Text, TextInput, Pressable, Alert } from 'react-native';
+import { View, Text, TextInput, Pressable, Alert, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const navigation = useNavigation();
 
   const SERVER_URL = 'https://nxtgm-server.onrender.com/api/login'; // Replace with your IP
 
   const handleLogin = async () => {
+    setIsLoading(true)
     if (!username || !password) {
       Alert.alert('Error', 'Please enter email and password');
       return;
     }
-  
+
     try {
       const response = await fetch(SERVER_URL, {
         method: 'POST',
@@ -23,25 +25,27 @@ export default function Login() {
         },
         body: JSON.stringify({ username, password }),
       });
-  
+
       if (!response.ok) {
         const errorData = await response.json();
         Alert.alert('Login Failed', errorData.message || 'Invalid credentials');
+        setIsLoading(false)
         return;
       }
-  
+
       const data = await response.json();
       console.log('Login success:', data);
       console.log('Username:', data?.username);
-      console.log('userId:', data?.userId); 
-  
+      console.log('userId:', data?.userId);
+
       Alert.alert('Success', data.message || 'Login successful');
-  
+
       navigation.navigate('Home', { username: data?.username, userid: data?.userId });
-  
+      setIsLoading(false)
     } catch (error) {
       console.error('Login error:', error);
       Alert.alert('Error', 'Could not connect to the server');
+      setIsLoading(false)
     }
   };
 
@@ -64,11 +68,15 @@ export default function Login() {
         value={password}
         onChangeText={setPassword}
       />
-      <Pressable
-        className="w-[200px] items-center rounded-xl bg-green-700"
-        onPress={handleLogin}>
-        <Text className="p-3 text-white">Login</Text>
-      </Pressable>
+      {isLoading ? (
+        <View className="w-[200px] items-center rounded-xl bg-green-700">
+          <ActivityIndicator className='p-3' color="ffffff"/>
+        </View>
+      ) : (
+        <Pressable className="w-[200px] items-center rounded-xl bg-green-700" onPress={handleLogin}>
+          <Text className="p-3 text-white">Login</Text>
+        </Pressable>
+      )}
     </View>
   );
 }
